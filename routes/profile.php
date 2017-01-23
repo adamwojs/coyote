70210b5255d3b531a7473fd28ebf8ae59f622ae0
Adam Wójs <adam@wojs.pl>
2017-01-23 23:05:39 +0100

    Użytkownicy - Kontroler, konfiguracja oraz widok #111

--- routes/profile.php
+++ routes/profile.php
@@ -1,6 +1,9 @@
 <?php
 
 /** @var $this \Illuminate\Routing\Router */
+$this->get('Profile', ['uses' => 'Profile\ListController@index', 'as' => 'profile.list']);
 $this->get('Profile/{user}/History', ['uses' => 'Profile\HomeController@history', 'as' => 'profile.history']);
 // ta regula musi znalezc sie na samym koncu
 $this->get('Profile/{user}/{tab?}', ['uses' => 'Profile\HomeController@index', 'as' => 'profile']);
+
+
